1119d5338a7ad06c636db402e276b4a2c23feab9
leonel_castaneda <leonel_castaneda@condenast.com>
2023-03-11 23:03:05 -0600

    update contact section padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,13 +32,7 @@ module.exports = {
         secondary: "#13C296",
         "body-color": "#637381",
         warning: "#FBBF24",
-      },
-      boxShadow: {
-        input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
-        pricing: "0px 39px 23px -27px rgba(0, 0, 0, 0.04)",
-        "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
-        testimonial: "0px 60px 120px -20px #EBEFFD",
-      },
+      }
     },
   },
   variants: {
